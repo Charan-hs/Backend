8c8ea91af22e7a2ff9ee7eadeca452d5f236cccd
unknown <charanengg08@gmail.com>
2020-10-29 13:18:24 +0530

    to Deploy59

---
+++ schoolreact/build/precache-manifest.0d93dde51f0670b8e4971ae4463ae7b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23967e5222530245e7916ee0f9a5f4e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f8b2f6727efc990e9ac",
+    "url": "/static/css/main.c2e1e05f.chunk.css"
+  },
+  {
+    "revision": "db8df7d7144e9fade10e",
+    "url": "/static/js/2.1fa80385.chunk.js"
+  },
+  {
+    "revision": "0dc713a05373e5c9a88cb999f31ae6e4",
+    "url": "/static/js/2.1fa80385.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f8b2f6727efc990e9ac",
+    "url": "/static/js/main.ccb6c05d.chunk.js"
+  },
+  {
+    "revision": "32b7fc91f51f50c96a66",
+    "url": "/static/js/runtime-main.0cd891d4.js"
+  }
+]);
